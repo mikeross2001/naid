b8df1cfa0a6cfb8d22e8c1b698a0add69729c196
M R <mr@Ms-MacBook-Pro.local>
2026-01-25 15:07:45 +0700

    Add script to remove unwanted restaurants and bars

---
+++ supabase/remove-restaurants.sql
@@ -0,0 +1,36 @@
+-- Remove restaurants and bars from database
+
+-- BARS TO REMOVE
+DELETE FROM public.restaurants WHERE slug = 'goldsmith-bar';
+DELETE FROM public.restaurants WHERE slug = 'marie';
+DELETE FROM public.restaurants WHERE slug = 'prumplum-stand';
+DELETE FROM public.restaurants WHERE slug = 'cul-de-sac';
+DELETE FROM public.restaurants WHERE slug = 'paper-plane-project';
+DELETE FROM public.restaurants WHERE slug = 'wasteland';
+DELETE FROM public.restaurants WHERE slug = 'la-copita';
+DELETE FROM public.restaurants WHERE slug = 'sanctuary-bangkok';
+DELETE FROM public.restaurants WHERE slug = 'residents-only';
+DELETE FROM public.restaurants WHERE slug = 'the-national-bar';
+DELETE FROM public.restaurants WHERE slug = 'pickle-bkk';
+DELETE FROM public.restaurants WHERE slug = 'eau-de-toilette';
+DELETE FROM public.restaurants WHERE slug = 'sin-rooftop-bar';
+DELETE FROM public.restaurants WHERE slug = 'muan-bangkok';
+DELETE FROM public.restaurants WHERE slug = 'coucou';
+DELETE FROM public.restaurants WHERE slug = 'the-red-door';
+DELETE FROM public.restaurants WHERE slug = 'chinatown-yacht-club';
+
+-- RESTAURANTS TO REMOVE
+DELETE FROM public.restaurants WHERE slug = 'paste-bangkok';
+DELETE FROM public.restaurants WHERE slug = 'krua-apsorn';
+DELETE FROM public.restaurants WHERE slug = 'after-you';
+DELETE FROM public.restaurants WHERE slug = 'eighty-twenty';
+DELETE FROM public.restaurants WHERE slug = 'err';
+DELETE FROM public.restaurants WHERE slug = 'smokin-pug';
+DELETE FROM public.restaurants WHERE slug = 'daniel-thaiger';
+DELETE FROM public.restaurants WHERE slug = 'broccoli-revolution';
+DELETE FROM public.restaurants WHERE slug = 'veganerie';
+DELETE FROM public.restaurants WHERE slug = 'woo-galbi';
+DELETE FROM public.restaurants WHERE slug = 'china-table';
+DELETE FROM public.restaurants WHERE slug = 'tk-seafood';
+DELETE FROM public.restaurants WHERE slug = 'gaggan-anand';
+DELETE FROM public.restaurants WHERE slug = 'jaime';
